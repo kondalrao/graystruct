f1bfbb449a209d6c4036f5b21cd9e2dacddd70a1
Simon Jagoe <simon@simonjagoe.com>
2015-02-28 22:38:39 +0000

    Rearrange modules

--- graystruct/rabbit.py
+++ graystruct/rabbit.py
@@ -5,3 +5,10 @@
 # This software may be modified and distributed under the terms
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 from __future__ import absolute_import
+from graypy.rabbitmq import GELFRabbitHandler as BaseGELFRabbitHandler
+
+from .handler import _CompressHandler
+
+
+class GELFRabbitHandler(_CompressHandler, BaseGELFRabbitHandler):
+    pass
